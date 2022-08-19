e9536f77bf37b1135c0d026ea011c33ca3dd28e8
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-08-19 21:57:35 +0300

    I have added my work experience, certification and courses, and web technologies that I have learned. And I used Sass on the stylesheet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-$(function(){
-    var currentDate = new Date();
-    $('#currentYear').text(currentDate.getFullYear());
-})
+$(function () {
+  var currentDate = new Date();
+  $("#currentYear").text(currentDate.getFullYear());
+});
+
+// JavaScript
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
+window.onscroll = function () {
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
